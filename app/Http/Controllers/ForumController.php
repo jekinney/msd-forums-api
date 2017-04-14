fe0342e8063937d91752eaa075716eeede968317
MSD\jkinney <jkinney@MSDist.com>
2017-04-14 15:05:18 -0500

    added db to command

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Thread;
 use App\Channel;
-use App\Category;
 use App\Fractal\Threads;
 use App\Fractal\Channels;
 use App\Fractal\Categories;
@@ -15,7 +14,7 @@ class ForumController extends Controller
     public function index(Category $category)
     {
     	$threads = fractal(Thread::with('channel')->withCount('replies')->latest()->paginate(10), new Threads);
-    	$channels = fractal(Channel::get(), new Channels);
+    	$channels = fractal(Channel::where('is_hidden', false)->orderBy('order', 'asc')->get(), new Channels);
 
     	return response()->json(collect(['threads' => $threads, 'channels' => $channels]));
     }
