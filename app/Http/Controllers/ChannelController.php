033b428a0f765023a740514b66ff9a19fbab3e5a
vagrant <vagrant@homestead>
2017-04-18 15:31:32 +0000

    updates to channel

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -27,8 +27,10 @@ class ChannelController extends Controller
     public function all(Channel $channel)
     {
         $channel = $channel->withCount('threads', 'replies')->orderBy('order', 'asc')->get();
-        return $channel->threads->lastest()->first(),
-        return fractal($channel->withCount('threads', 'replies')->orderBy('order', 'asc')->get(), new AllChannels)->respond();
+        return $channel->threads->lastest()->first();
+
+
+        //return fractal($channel->withCount('threads', 'replies')->orderBy('order', 'asc')->get(), new AllChannels)->respond();
     }
 
     /**
