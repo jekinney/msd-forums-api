23dcd385ed8cc824ed8f732fb02f2e268c418a88
vagrant <vagrant@homestead>
2017-04-20 21:01:01 +0000

    updated to attachments

--- app/Attachment.php
+++ app/Attachment.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-	protected $fillable = ['user_id', 'path', 'attachable_id', 'attachable_type', 'hidden'];
+	protected $fillable = ['user_id', 'name', 'path', 'attachable_id', 'attachable_type', 'hidden'];
 
     /**
      * Get all of the owning attachable models.
@@ -37,7 +37,7 @@ class Attachment extends Model
 
         $class = 'App\\'.studly_case($request->type);
         $class = new $class();
-        $class->find($request->id)->attachments()->create(['name' => $file->getClientOriginalName(), 'full_path' => $path]);
+        $class->find($request->id)->attachments()->create(['name' => $file->getClientOriginalName(), 'path' => $path]);
         
         return $class;
     }
