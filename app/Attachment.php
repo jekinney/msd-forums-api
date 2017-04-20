1c4544fd39e73f7fb73585c25d52e66d50bd8028
vagrant <vagrant@homestead>
2017-04-20 19:09:19 +0000

    updated to attachments

--- app/Attachment.php
+++ app/Attachment.php
@@ -3,13 +3,12 @@
 namespace App;
 
 use Illuminate\Http\File;
-use App\Google\FileStorage;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-	protected $fillable = ['user_id', 'attachable_id', 'attachable_type', 'hidden'];
+	protected $fillable = ['user_id', 'path', 'attachable_id', 'attachable_type', 'hidden'];
 
     /**
      * Get all of the owning attachable models.
@@ -20,20 +19,12 @@ class Attachment extends Model
     }
 
     /**
-     * Upload image from tinymce plugin
-     */
-    public function uploadImage($request)
-    {
-        return str_replace('public', '', $request->file('file')->store('public/images/forums'));
-    }
-
-    /**
     * Upload file (word, pdf, etc) attached to a thread or reply
     */
     public function uploadFiles($request)
     {
         $file = $request->file('attachment')->store('public/attachments/forums');
         return $file;
-        $request->type.::find($request->id)->attachments()->create(['path' => str_replace('public', '', $file)]);
+        //$request->type.::find($request->id)->attachments()->create(['path' => str_replace('public', '', $file)]);
     }
 }
