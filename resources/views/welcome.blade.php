8fded35f2c7bae35c4e38680a4dd0f1a4166d46f
vagrant <vagrant@homestead>
2017-05-01 13:47:12 +0000

    updating Notifications

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,8 +81,6 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-                
-                {{ App\Channel::first() }}
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
