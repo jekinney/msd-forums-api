6e497d64508c685182b38bfcb86de0b0bf39f3c3
vagrant <vagrant@homestead>
2017-05-24 15:15:37 +0000

    faorites errors

--- app/Collections/UserData.php
+++ app/Collections/UserData.php
@@ -6,6 +6,8 @@ class UserData
 {
 	public function reply($user)
 	{
+		$following = ['threads', 'channels'];
+
 		foreach($user->followed as $follow) {
 			if (str_contains($follow->followable_type, 'Thread')) {
 				$following['threads'][] = $follow->followable_id;
@@ -16,6 +18,7 @@ class UserData
 
 		return [
 			'id' => $user->id,
+			'nav_id' => $user->nav_id,
 			'name' => $user->name,
 			'company' => $user->company,
 			'type' => $user->type,
--- app/Http/Controllers/Forums/FollowedController.php
+++ app/Http/Controllers/Forums/FollowedController.php
@@ -13,40 +13,42 @@ use App\Forums\Collections\ChannelDetails;
 
 class FollowedController extends Controller
 {
-	protected $followed;
+	   protected $followed;
 
-	function __construct(Followed $followed)
-	{
-		$this->followed = $followed;
-	}
+	   function __construct(Followed $followed)
+	   {
+		      $this->followed = $followed;
+	   }
 
     public function index($userId, ThreadList $threadList, ChannelDetails $channelDetails)
     {
-  		$followeds = $this->followed->with('followable')->where('user_id', $userId)->get();
+  		  $followeds = $this->followed->with('followable')->where('user_id', $userId)->get();
+        $threads = [];
+        $channels = []; 
 
-  		foreach($followeds as $followed) {
-  			$type = strtolower(class_basename($followed->followable));
-  			if($type == 'thread') {
-  				$threads[] = $threadList->reply($followed->followable->load('user', 'channel'));
-  			} elseif($type == 'channel') {
-  				$channels[] = $channelDetails->reply($followed->followable->load('category', 'threads'));
-  			}
-  		}
+  		  foreach($followeds as $followed) {
+  			   $type = strtolower(class_basename($followed->followable));
+  			   if($type == 'thread') {
+  				    $threads[] = $threadList->reply($followed->followable->load('user', 'channel'));
+  			   } elseif($type == 'channel') {
+  				    $channels[] = $channelDetails->reply($followed->followable->load('category', 'threads'));
+  			   }
+  		  }
 
-  		return response()->json(['threads' => $threads, 'channels' => $channels]);
+  		  return response()->json(['threads' => $threads, 'channels' => $channels]);
     }
 
     public function thread(Request $request, Thread $thread, UserData $userData)
     {
-    	$user = $this->followed->toggle($thread->with('followed')->find($request->thread_id), $request->user_id);
+    	 $user = $this->followed->toggle($thread->with('followed')->find($request->thread_id), $request->user_id);
 
-    	return response()->json($userData->reply($user->load('followed')));
+    	  return response()->json($userData->reply($user->load('followed')));
     }
 
     public function channel(Request $request, Channel $channel, UserData $userData)
     {
-    	$user = $this->followed->toggle($channel->with('followed')->find($request->channel_id), $request->user_id);
+    	   $user = $this->followed->toggle($channel->with('followed')->find($request->channel_id), $request->user_id);
 
-    	return response()->json($userData->reply($user->load('followed')));
+    	   return response()->json($userData->reply($user->load('followed')));
     }
 }
